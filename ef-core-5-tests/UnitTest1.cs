6ad5b2d5f73d63da35b39dfc2d591ac172946982
Michael McCann <michael@mccanns.org>
2020-10-15 10:50:55 -0700

    wip

--- ef-core-5-tests/UnitTest1.cs
+++ ef-core-5-tests/UnitTest1.cs
@@ -1,13 +1,25 @@
-using System;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+using FluentAssertions;
 using Xunit;
 
 namespace ef_core_5_tests
 {
+    public class AutoMockAttribute : AutoDataAttribute
+    {
+        public AutoMockAttribute()
+            : base(() => new Fixture().Customize(new AutoMoqCustomization()))
+        {
+        }
+    }
+
     public class UnitTest1
     {
-        [Fact]
+        [Theory, AutoMock]
         public void Test1()
         {
+            true.Should().Be(true);
         }
     }
 }
